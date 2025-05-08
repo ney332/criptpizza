8b639a2f60ffba3361449b757cfe2c7e1a604f98
ney <lorransantos059@gmail.com>
2025-05-08 15:34:43 +0300

    n

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header classname="App-header">
+      <header className="App-header">
         <h2>CriptoPizza</h2>
         <nav className="header_nav">
           <a>entrar</a>
